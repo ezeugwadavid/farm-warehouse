2afebb9287943815f63151d4bf260e1ab879b329
Ezeugwa David <ezeugwadavid@EzeugwaDavids-MacBook-Pro.local>
2024-02-20 11:21:26 +0100

    commit

--- src/services/register.js
+++ src/services/register.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const registerUser = async(data) => {
-  const result = await axios.post("http://fwh-backend.vercel.app/api/users/signup", data);
+  const result = await axios.post("https://fwh-backend.vercel.app/api/users/signup", data);
   return result;
 };
 
